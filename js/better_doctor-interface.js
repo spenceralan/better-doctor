d8261e58d3fc00edab529253a579f71c93e37108
Spencer Alan <spencer.alan@icloud.com>
2017-05-26 12:45:13 -0700

    add doctor specialty

--- js/better_doctor-interface.js
+++ js/better_doctor-interface.js
@@ -5,11 +5,15 @@ let displayDoctors = function(doctors) {
     $(".results").append(`<h1>there were no results. no one can help you.</h1>`);
   } else {
     doctors.forEach(function(doctor){
-      let first_name = doctor.profile.first_name
-      let last_name = doctor.profile.last_name
+      let first_name = doctor.profile.first_name;
+      let last_name = doctor.profile.last_name;
       $(".results").append(`<h1>Doctor ${first_name} ${last_name}</h1>`);
+      doctor.specialties.forEach(function(specialty){
+        let specialty_ies = doctor.specialties.length > 1 ? "Specialties" : "Specialty";
+        $(".results").append(`<h3>${specialty_ies}: ${specialty.name}</h3>`);
+      });
     });
-  };
+  }
 };
 
 $(document).ready(function(){
--- js/better_doctor.js
+++ js/better_doctor.js
@@ -7,6 +7,7 @@ class Doctors {
 
   byCondition(displayDoctors) {
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${this.condition}&location=or-portland&user_location=45.523%2C-122.676&skip=0&limit=10&user_key=${apiKey}`).then(function(query){
+      console.log(query.data);
       displayDoctors(query.data);
     }).fail(function(query) {
       console.log("fail");
