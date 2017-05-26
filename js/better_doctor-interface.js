b343fefdcd6a5153f089bf2335f2c87b23101af1
Spencer Alan <spencer.alan@icloud.com>
2017-05-26 14:51:09 -0700

    add phone numbers and address for the doctors

--- js/better_doctor-interface.js
+++ js/better_doctor-interface.js
@@ -2,7 +2,9 @@ let Doctors = require("./../js/better_doctor.js").doctorsModule;
 
 let error = function() {
     $(".results").append(`<h1>something went wrong. just like your condition.</h1>`);
-}
+};
+
+
 
 let displayDoctors = function(doctors) {
   if (doctors.length === 0) {
@@ -11,12 +13,16 @@ let displayDoctors = function(doctors) {
     doctors.forEach(function(doctor){
       let first_name = doctor.profile.first_name;
       let last_name = doctor.profile.last_name;
-      $(".results").append(`<h1>Doctor ${first_name} ${last_name}</h1>`);
+      let phone_number = doctor.practices[0].phones[0].number
+      let street_address = doctor.practices[0].visit_address.street
+      let city = doctor.practices[0].visit_address.city
+      let state = doctor.practices[0].visit_address.state
+      let zipcode = doctor.practices[0].visit_address.zip
 
-      doctor.specialties.forEach(function(specialty){
-        let specialty_ies = doctor.specialties.length > 1 ? "Specialties" : "Specialty";
-        $(".results").append(`<h4>${specialty_ies}: ${specialty.name}</h4>`);
-      });
+      $(".results").append(`<h1>Doctor ${first_name} ${last_name}</h1>`);
+      $(".results").append(`<h4>${phone_number}</h4>`);
+      $(".results").append(`<h4>${street_address}</h4>`);
+      $(".results").append(`<h4>${city}, ${state} ${zipcode}</h4>`);
 
     });
   }
@@ -24,14 +30,23 @@ let displayDoctors = function(doctors) {
 
 $(document).ready(function(){
 
-  $("#get-doctors").click(function(event){
+  $("#get-doctors-by-condition").click(function(event){
     event.preventDefault();
     $(".results").empty();
 
-    let condition = $("#condition").val();
-    doctors = new Doctors(condition);
-
+    doctors = new Doctors({condition: $("#condition").val()});
+    $("#condition").val("")
     doctors.byCondition(displayDoctors);
+
+  });
+
+  $("#get-doctors-by-specialty").click(function(event){
+    event.preventDefault();
+    $(".results").empty();
+
+    doctors = new Doctors({specialty: $("#specialty").val()});
+    $("#specialty").val("")
+    doctors.bySpecialty(displayDoctors);
 
   });
 
--- js/better_doctor.js
+++ js/better_doctor.js
@@ -1,12 +1,22 @@
 const apiKey = require("./../.env").apiKey;
 
 class Doctors {
-  constructor(condition) {
-    this.condition = condition;
+  constructor(attributes) {
+    this.condition = attributes.condition;
+    this.specialty = attributes.specialty;
   }
 
   byCondition(displayDoctors) {
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${this.condition}&location=or-portland&user_location=45.523%2C-122.676&skip=0&limit=10&user_key=${apiKey}`).then(function(query){
+      console.log(query.data);
+      displayDoctors(query.data);
+    }).fail(function() {
+      error();
+    });
+  }
+
+  bySpecialty(displayDoctors) {
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${this.specialty}&location=or-portland&user_location=45.523%2C-122.676&skip=0&limit=20&user_key=${apiKey}`).then(function(query){
       console.log(query.data);
       displayDoctors(query.data);
     }).fail(function() {
