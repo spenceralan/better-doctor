709888747929609728220d5360cfcb0a1cf48f2d
Spencer Alan <spencer.alan@icloud.com>
2017-05-26 16:20:45 -0700

    remove console logs

--- js/better_doctor.js
+++ js/better_doctor.js
@@ -8,7 +8,6 @@ class Doctors {
 
   byCondition(displayDoctors) {
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${this.condition}&location=or-portland&user_location=45.523%2C-122.676&skip=0&limit=10&user_key=${apiKey}`).then(function(query){
-      console.log(query.data);
       displayDoctors(query.data);
     }).fail(function() {
       error();
@@ -17,7 +16,6 @@ class Doctors {
 
   bySpecialty(displayDoctors) {
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${this.specialty}&location=or-portland&user_location=45.523%2C-122.676&skip=0&limit=20&user_key=${apiKey}`).then(function(query){
-      console.log(query.data);
       displayDoctors(query.data);
     }).fail(function() {
       error();
